dc5ad92471a6d112ee6850413e3fe777e27fac4d
EvgeniUPS <evgeniups@gmail.com>
2018-08-04 17:13:37 +0300

    06. String

---
+++ sorax/js/06.js
@@ -0,0 +1,33 @@
+console.log("Some Srting");
+console.log('Some "long" string');
+
+console.log("Hello".length);
+
+console.log("Another \n\tvery long \n\t\t string\\");
+
+var string = "Hello";
+console.log(string + " world");
+
+string = "Sometimes the same is different";
+console.log(string.length);
+console.log(string.charAt(0));
+console.log(string.charAt(string.length - 1));
+
+console.log(string.substring(10));
+console.log(string.substring(10, 21));
+
+console.log(string.slice(-10));
+
+console.log(string.substr(14, 4));
+
+console.log(string.indexOf("i"));
+console.log(string.lastIndexOf("me"));
+
+console.log(string.replace("is", "is not"));
+
+console.log(string.split(" "));
+
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+console.log(string[4]);
