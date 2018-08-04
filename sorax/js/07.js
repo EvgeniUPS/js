493363af9393ba4908ba085769f8c88421a1b486
EvgeniUPS <evgeniups@gmail.com>
2018-08-04 17:54:16 +0300

    07. Boolean

---
+++ sorax/js/07.js
@@ -0,0 +1,33 @@
+console.log(true);
+console.log(false);
+
+console.log(5 === 5);
+console.log(5 === 6);
+
+console.log(Boolean(5));
+console.log("");
+
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log("");
+
+console.log(true && false);
+console.log(true && true);
+
+console.log(true || false);
+console.log(false || false);
+
+console.log(!true);
+console.log("");
+
+var a = 0;
+isTrue = false;
+isTrue && (a = 5);
+console.log(a);
+
+var someString = "Non-empty string";
+var newString = someString || "Default String";
+console.log(newString);
