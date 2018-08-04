441b0c03343833dfd65235b108460296e72ea21b
EvgeniUPS <evgeniups@gmail.com>
2018-08-04 16:25:25 +0300

    05. Number

---
+++ sorax/js/04.js
@@ -0,0 +1,30 @@
+var _var = 0,
+  $var = 1,
+  var6 = 2,
+  myvar,
+  myVar;
+
+var myNumber = 213123,
+  mySring = "Some String",
+  myBool = true,
+  myNull = null,
+  myUndef = undefined;
+
+console.log(typeof myNumber);
+console.log(typeof mySring);
+console.log(typeof myBool);
+console.log(typeof myNull);
+console.log(typeof myUndef);
+
+console.log("Sorax".toUpperCase());
+
+var obj = { name: "Sorax" },
+  array = [1, 2, 3],
+  regexp = /w+/g,
+  func = function() {};
+console.log("");
+
+console.log(typeof obj);
+console.log(typeof array);
+console.log(typeof regexp);
+console.log(typeof func);
---
+++ sorax/js/05.js
@@ -0,0 +1,87 @@
+console.log(5);
+console.log(0);
+console.log(300000);
+console.log(0xfffcc);
+console.log(0xfffcc);
+console.log(0345);
+console.log(0348);
+
+console.log(12.45);
+console.log(0.89);
+console.log(1.34e4);
+console.log(4.5e-30);
+console.log("");
+
+var N = new Number(4000);
+console.log(typeof N);
+console.log(N.toFixed(2));
+
+var n = 12.67;
+console.log(typeof n);
+console.log(n.toFixed(2));
+
+console.log((78).toFixed(2));
+
+console.log(n.toExponential(4));
+
+console.log(n.toPrecision(3));
+
+console.log(+10);
+console.log(-10);
+
+var i = 10;
+console.log(++i);
+console.log(i);
+console.log(i++);
+console.log(i);
+
+console.log(5 + 10);
+console.log(7 - 3);
+console.log(5 * 9);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log("");
+
+var n = 100;
+n = n + 20;
+console.log(n);
+n += 30;
+console.log(n);
+n /= 3;
+console.log(n);
+
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(10 >= 10);
+console.log(8 <= 10);
+
+console.log(10 === 10);
+console.log(10 !== 10);
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log("");
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(3, 5));
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(
+  100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+);
+console.log(typeof Infinity);
+console.log(Infinity * 5);
+console.log(Infinity + 10);
+console.log(Infinity * Infinity);
+console.log("");
+
+console.log(5 / 0);
+console.log(0 / 0);
+console.log(Infinity / Infinity);
+console.log(Math.sqrt(-10));
+console.log(NaN === NaN);
+console.log("");
+
+console.log(0.2 + 0.1);
+console.log(10000000000000008 + 10000000000000001);
