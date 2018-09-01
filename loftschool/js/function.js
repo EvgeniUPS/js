10529eef4386426221bcd21981489d78137986c4
EvgeniUPS <evgeniups@gmail.com>
2018-09-01 17:57:52 +0300

    function

--- loftschool/js/function.js
+++ loftschool/js/function.js
@@ -28,8 +28,42 @@ console.log(fn2(2, 2)); */
 function fn(otherFn) {
   otherFn();
 }
-var a = 3;
+// function declaration
 function fn2() {
   console.log("Привет");
 }
 fn(fn2);
+////
+function fn(otherFn) {
+  otherFn();
+}
+// function expression
+fn(function fn2() {
+  console.log("Привет");
+});
+
+////////////////////////////////
+// стрелочные функции
+/* function fn3() {
+  console.log("Привет");
+}
+var fn4 = a => {
+  console.log("Hello " + a);
+  console.log("Hello " + a);
+  console.log("Hello " + a);
+};
+var fn5 = a => console.log("Hello " + a);
+fn4("world");
+fn5("world");
+fn(() => console.log("hi"));
+var fn6 = () => console.log("eee");
+fn(fn6);
+fn6(); */
+////////////////
+var a = [1, 2, 3, 4, 5];
+
+var a2 = a.map(function(number) {
+  return number * number;
+});
+console.log(a);
+console.log(a2);
